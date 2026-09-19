4e10f533b9317b1a5bb3c0d21f605cf1e6381890
Harsh Raundal <dkingston0000@gmail.com>
2026-09-19 20:00:47 +0530

    Create HollowSquare.java

---
+++ HollowSquare.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class first
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int b = 6;
+//Printing hollow square using nested for loops
+        for(int i = 1;i<=a;i++)
+        {
+            for(int j = 1;j<=b;j++)
+            {
+                if(i == 1 || j == 1 ||i == a|| j == b )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
