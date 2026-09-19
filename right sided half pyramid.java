4a23e82a80606df470f7ed6adf8fc06c4b9cf889
Harsh Raundal <dkingston0000@gmail.com>
2026-09-19 20:01:58 +0530

    Implement right sided half pyramid in Java
    Added a Java program to print a right-sided half pyramid pattern.

---
+++ right sided half pyramid.java
@@ -0,0 +1,23 @@
+class first
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int b = 6;
+
+        for(int i = 1;i<=a;i++)
+        {
+            for(int j = 1;j<=a-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j = 1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.print("Harsh");
+    }
+}
